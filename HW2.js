2bd51c9aa78cde7f827a14cbf747c336451521ef
Lasha-Adeishvili <adeishvili.lasha26@gmail.com>
2023-04-27 16:23:03 +0400

    problem 1,3,4

---
+++ HW2.js
@@ -0,0 +1,55 @@
+let x = 5;
+console.log(x + 2);
+console.log(x - 6); // -1
+console.log(x * 30); // 150
+console.log(x / 2); // 2.5
+console.log(x % 5) //0
+console.log(x ** 2) //25
+
+
+/*
+assigment operators
+ */
+
+let z = 5;
+console.log(z)
+z = z + 5; //15
+z -=6; //11
+z *= 30; //330
+z /= 2; //115
+z %= 5; //0
+z **= 3; //0
+
+console.log(z)
+
+/*
+Comparison operators
+*/
+
+
+
+
+/* Conditionals */
+
+let firstName = "Lasha";
+let age = 5;
+let price = 35;
+if (age < 18){
+    console.log('Underage, price is free')
+} else if (firstName.toLocaleLowerCase() === 'James'){}
+else if (age === 25){
+    console.log("discount by 20%")
+}
+
+
+
+
+
+
+/* 
+Arrays 
+*/
+let Q = -100;
+let W = 100;
+
+
